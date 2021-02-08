59be4bf958e0ee5e2c4d60dcd1807ecf8ae5c60b
D-dn <dashoonya@ukr.net>
2021-02-08 22:19:50 +0200

    tasks 1-5

---
+++ Daria_Homework_4.py
@@ -0,0 +1,30 @@
+####################################
+# my_list = [2, 400, 52, 560, 82, 14]
+# for symbol in my_list:
+#     if symbol > 100:
+#         print(symbol)
+
+#################################### - doesn't work
+# my_list = [2, 400, 52, 560, 82, 14]
+# for symbol in my_list:
+#     my_result = [
+#         for symbol in my_list
+#         if symbol > 100]:
+#         print(my_result)
+
+####################################
+# my_list = [2, 22, 54, 124, 1, 2]
+# if len(my_list) < 2:
+#     my_list.append(0)
+# else:
+#     my_list.append(my_list[-1] + my_list[-2])
+# print(my_list)
+
+# ####################################
+# value = input("Ввелите число с точкой:")
+#
+# try:
+#     value = float(value)
+#     print(value ** -1)
+# except (ValueError, ZeroDivisionError):
+#     print("Неверный ввод")
