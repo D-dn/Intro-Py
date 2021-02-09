bcfe7f900ec52804222d22a5b21c42583b45abf7
D-dn <dashoonya@ukr.net>
2021-02-09 11:35:04 +0200

    tasks 1-6

--- Daria_Homework_4.py
+++ Daria_Homework_4.py
@@ -1,18 +1,18 @@
-####################################
+#################################### №1
 # my_list = [2, 400, 52, 560, 82, 14]
 # for symbol in my_list:
 #     if symbol > 100:
 #         print(symbol)
 
-#################################### - doesn't work
+#################################### №2
 # my_list = [2, 400, 52, 560, 82, 14]
+# my_result = []
 # for symbol in my_list:
-#     my_result = [
-#         for symbol in my_list
-#         if symbol > 100]:
-#         print(my_result)
+#         if symbol > 100:
+#             my_result.append(symbol)
+# print(my_result)
 
-####################################
+#################################### №3
 # my_list = [2, 22, 54, 124, 1, 2]
 # if len(my_list) < 2:
 #     my_list.append(0)
@@ -20,7 +20,7 @@
 #     my_list.append(my_list[-1] + my_list[-2])
 # print(my_list)
 
-# ####################################
+# #################################### №4
 # value = input("Ввелите число с точкой:")
 #
 # try:
@@ -28,3 +28,22 @@
 #     print(value ** -1)
 # except (ValueError, ZeroDivisionError):
 #     print("Неверный ввод")
+
+# #################################### №5
+
+# my_list = [1, 11, 20, 100]
+# for index, symbol in enumerate(my_list):
+#     if not (index + symbol) % 2:
+#         print(symbol)
+
+# #################################### №6
+# my_list = [1, 11, 20, 100]
+# my_list_2 = [3, 33, 30, 300]
+# for index in range(len(my_list)):
+#         print(my_list[index], my_list_2[index])
+#
+# ###############  or  ##################### №6
+# my_list = [1, 11, 20, 100]
+# my_list_2 = [3, 33, 30, 300]
+# for index, symbol in enumerate(my_list):
+#     print(my_list[index], my_list_2[index])
