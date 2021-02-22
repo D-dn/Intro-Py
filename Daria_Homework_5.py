bc463c9cc7c2933e008e52b8a6cb36327f0a4ec9
D-dn <78500995+D-dn@users.noreply.github.com>
2021-02-22 12:00:39 +0200

    Update Daria_Homework_5.py

--- Daria_Homework_5.py
+++ Daria_Homework_5.py
@@ -21,11 +21,9 @@ for symbol in my_str:
         break
 print(my_value)
 
-
 # # 3a. Даны списки my_list_1 и my_list_2.
 # # Создать список my_result в который вначале поместить
 # # элементы на четных местах из my_list_1, а потом все элементы на нечетных местах из my_list_2.
-
 
 my_list_1 = [0, 8, 0, 5, 0]
 my_list_2 = [5, 1, 9, 1, 8]
@@ -51,16 +49,13 @@ for symbol in my_list_2:
         my_result.append(symbol)
 print(my_result)
 
-
 # # 4. Дан список my_list. СОЗДАТЬ НОВЫЙ список new_list у которого первый элемент из my_list
 # # стоит на последнем месте. Если my_list [1,2,3,4], то new_list [2,3,4,1]
-# # ? не пойму, почему 2 квадрат
 
 my_list = [1, 2, 3, 4]
 new_list = my_list[1:]
 new_list.append(my_list[0])
 print(new_list)
-
 
 # # 5.Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место.
 # # [1,2,3,4] -> [2,3,4,1]. Пересоздавать список нельзя! (используйте метод pop)
@@ -68,7 +63,6 @@ print(new_list)
 my_list = [1, 2, 3, 4, 5, 8, 9, 11]
 my_list.append(my_list.pop(0))
 print(my_list)
-
 
 # # # 6. Дана строка в которой есть числа (разделяются пробелами).
 # # # Например "43 больше чем 34 но меньше чем 56". Найти сумму ВСЕХ ЧИСЕЛ (А НЕ ЦИФР) в этой строке.
@@ -86,7 +80,6 @@ print(sum(my_result))
 # Если строка содержит нечетное количество символов, пропущенный второй символ последней пары должен
 # # быть заменен подчеркиванием ('_'). Примеры: 'abcd' -> ['ab', 'cd'], 'abcde' -> ['ab', 'cd', e_']
 
-
 my_str = "gfhjks;glhjghsdls;dfkjkp"
 my_list_1 = []
 if len(my_str) % 2:
@@ -96,33 +89,30 @@ for index, symbol in enumerate(my_str):
    my_list = my_list_1[::2]
 print(my_list)
 
-#
 # 8. Дана строка my_str в которой символы не повторяются и два символа l_limit, r_limit,
 # которые точно находятся в этой строке. Причем l_limit левее чем r_limit.
 # В переменную sub_str поместить часть строки между этими символами.
 # my_str = "My_long str", l_limit = "o", r_limit = "t" -> sub_str = "ng s"
 
-
-# my_str = "My_long str"
-# l_limit = "o"
-# r_limit = "t"
-# res_1 = my_str.find(l_limit) + 1
-# res_2 = my_str.find(r_limit)
-# sub_str = my_str[res_1:res_2]
-# print(sub_str)
+my_str = "My_long str"
+l_limit = "o"
+r_limit = "t"
+res_1 = my_str.find(l_limit) + 1
+res_2 = my_str.find(r_limit)
+sub_str = my_str[res_1:res_2]
+print(sub_str)
 
 # 9. Дана строка my_str в которой символы МОГУТ повторяться и два символа l_limit, r_limit,
 # которые точно находятся в этой строке. Причем l_limit левее чем r_limit.
 # В переменную sub_str поместить НАИБОЛЬШУЮ часть строки между этими символами.
 # my_str = "My long string", l_limit = "o", r_limit = "g" -> sub_str = "ng strin".
 
-# my_str = "My long string"
-# l_limit = "o"
-# r_limit = "g"
-# sub_str = my_str[my_str.find(l_limit) + 1 : my_str.rfind(r_limit)]
-# print(sub_str)
+my_str = "My long string"
+l_limit = "o"
+r_limit = "g"
+sub_str = my_str[my_str.find(l_limit) + 1 : my_str.rfind(r_limit)]
+print(sub_str)
 
-#
 # 10. Дан список чисел. Определите, сколько в этом списке элементов,
 # которые больше суммы двух своих соседей (слева и справа), и НАПЕЧАТАЙТЕ КОЛИЧЕСТВО таких элементов.
 # Крайние элементы списка никогда не учитываются, поскольку у них недостаточно соседей.
