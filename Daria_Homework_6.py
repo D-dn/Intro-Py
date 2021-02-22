3ad50f5187ec49408b0576d52c6dccc1280fc51a
D-dn <78500995+D-dn@users.noreply.github.com>
2021-02-22 12:26:16 +0200

    Update Daria_Homework_6.py
    task 1

--- Daria_Homework_6.py
+++ Daria_Homework_6.py
@@ -1,1 +1,9 @@
-
+##################################################
+my_list = ['qwe', 'rty', 'uio', 'opd', 'ljh']
+new_list = [0]
+for index, symbol in enumerate(my_list):
+    if not index % 2:
+        new_list.append(symbol)
+    elif index % 2:
+        new_list.append(symbol[::-1])
+print(new_list)
