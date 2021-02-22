0b02249b043ce706f0613138577fcd486b31cb7d
D-dn <78500995+D-dn@users.noreply.github.com>
2021-02-22 11:55:33 +0200

    Update Daria_Homework_5.py

--- Daria_Homework_5.py
+++ Daria_Homework_5.py
@@ -25,60 +25,62 @@ print(my_value)
 # # 3a. Даны списки my_list_1 и my_list_2.
 # # Создать список my_result в который вначале поместить
 # # элементы на четных местах из my_list_1, а потом все элементы на нечетных местах из my_list_2.
-# ? не пойму, почему 2 квадратных скобки
-#
-# my_list_1 = [0, 8, 0, 5, 0]
-# my_list_2 = [5, 1, 9, 1, 8]
-# my_result = []
-# my_result.append((my_list_1[::2]) + (my_list_2[1::2]))
-# print(my_result)
+
+
+my_list_1 = [0, 8, 0, 5, 0]
+my_list_2 = [5, 1, 9, 1, 8]
+my_result = my_list_1[::2]
+for index, symbol in enumerate(my_list_2):
+   if index % 2:
+      my_result.append(symbol)
+print(my_result)
 
 # # 3b. Даны списки my_list_1 и my_list_2. Создать список my_result в который
 # # вначале поместить четные элементы (ИМЕННО ЭЛЕМЕНТЫ) из my_list_1 и потом нечетные элементы из my_list_2.
 # # my_list_1 = [1,3,2,4,5], my_list_2 = [10, 20, 15, 25, 22] -> my_result [2, 4, 15, 25]
-#
-# my_list_1 = [1, 3, 2, 4, 5]
-# my_list_2 = [10, 20, 15, 25, 22]
-# my_result = []
-#
-# for symbol in my_list_1:
-#     if not symbol % 2:
-#         my_result.append(symbol)
-# for symbol in my_list_2:
-#     if symbol % 2:
-#         my_result.append(symbol)
-# print(my_result)
+
+my_list_1 = [1, 3, 2, 4, 5]
+my_list_2 = [10, 20, 15, 25, 22]
+my_result = []
+
+for symbol in my_list_1:
+    if not symbol % 2:
+        my_result.append(symbol)
+for symbol in my_list_2:
+    if symbol % 2:
+        my_result.append(symbol)
+print(my_result)
 
 
 # # 4. Дан список my_list. СОЗДАТЬ НОВЫЙ список new_list у которого первый элемент из my_list
 # # стоит на последнем месте. Если my_list [1,2,3,4], то new_list [2,3,4,1]
 # # ? не пойму, почему 2 квадрат
 
-# my_list = [1, 2, 3, 4]
-# new_list = my_list[1:]
-# new_list.append(my_list[0])
-# print(new_list)
+my_list = [1, 2, 3, 4]
+new_list = my_list[1:]
+new_list.append(my_list[0])
+print(new_list)
 
 #
 # # 5.Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место.
 # # [1,2,3,4] -> [2,3,4,1]. Пересоздавать список нельзя! (используйте метод pop)
-#
-# my_list = [1, 2, 3, 4, 5, 8, 9, 11]
-# my_list.append(my_list.pop(0))
-# print(my_list)
+
+my_list = [1, 2, 3, 4, 5, 8, 9, 11]
+my_list.append(my_list.pop(0))
+print(my_list)
 
 
 # # # 6. Дана строка в которой есть числа (разделяются пробелами).
 # # # Например "43 больше чем 34 но меньше чем 56". Найти сумму ВСЕХ ЧИСЕЛ (А НЕ ЦИФР) в этой строке.
 # # # Для данного примера ответ - 133.
 
-# my_string = "43 больше чем 34 но меньше чем 56"
-# my_result = []
-# my_string = my_string.split( )
-# for symbol in my_string:
-#     if symbol.isdigit():
-#         my_result.append(int(symbol))
-# print(sum(my_result))
+my_string = "43 больше чем 34 но меньше чем 56"
+my_result = []
+my_string = my_string.split( )
+for symbol in my_string:
+    if symbol.isdigit():
+        my_result.append(int(symbol))
+print(sum(my_result))
 
 # 7. Дана строка my_str. Разделите эту строку на пары из двух символов и поместите эти пары в список.
 # Если строка содержит нечетное количество символов, пропущенный второй символ последней пары должен
