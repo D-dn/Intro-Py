b23b19aa79cce7458dcbdb30845fb757b07f6fe1
D-dn <78500995+D-dn@users.noreply.github.com>
2021-03-01 22:55:51 +0200

    Create Daria_Homework_8.py
    task 2

---
+++ Daria_Homework_8.py
@@ -0,0 +1,33 @@
+#################################################################
+# 2. Даны списки names и domains (создать самостоятельно).
+# Написать функцию create_email для генерирования e-mail в формате:
+# фамилия.число_от_100_до_999@строка_букв_длинной_от_5_до_7_символов.домен
+# Обязательные параметры функции: names и domains.
+# Параметры по умолчанию: границы генерируемого числа (100, 999), границы генерируемой строки (5, 7)
+#
+# фамилию и домен брать случайным образом из заданных списков переданных в функцию в виде параметров.
+# Строку и число генерировать случайным образом.
+# Буквы в строке МОГУТ повторяться.
+#
+# Пример:
+# names = ["king", "miller", "kean"]
+# domains = ["net", "com", "ua"]
+# e_mail = create_email(domains, names)
+# print(e_mail)
+
+names = ["king", "miller", "kean"]
+domains = ["net", "com", "ua"]
+from random import randint, choice
+import string
+name = choice(names)
+domain = choice(domains)
+numb = randint(100, 999)
+len_str = randint(5, 7)
+alphabet = string.ascii_lowercase
+my_str = "".join([choice(alphabet) for _ in range(len_str)])
+
+def create_email(domains, names):
+    email = name + str(numb) + "@" + my_str + "." + domain
+    return email
+
+print(create_email(domains, names))
